095f60438db605497dd7ddecc4196e42ea424d98
nfwrhy-hue <nfwrhy@gmail.com>
2026-07-09 01:26:17 +0900

    Add files via upload

--- js/components/gnb.js
+++ js/components/gnb.js
@@ -2,6 +2,7 @@ export function initGnb(){
     const links = document.querySelectorAll(".gnb_menu a");
     links.forEach(link => {
         link.addEventListener("click", e => {
+            e.preventDefault(); // 스크롤 점프 방지
             links.forEach(item => item.classList.remove("active"));
             e.currentTarget.classList.add("active");
         });
